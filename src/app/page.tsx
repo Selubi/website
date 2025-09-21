a55f956c40728b956796fc076233171938d8bf89
Selubi <git@selubi.tech>
2025-09-21 13:43:52 +0900

    Very minimal homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,107 @@
-import Image from "next/image";
+import Experience from "@/components/Experience";
+import LinkWithIcon from "@/components/LinkWithIcon";
+import Posts from "@/components/Posts";
+import Projects from "@/components/Projects";
+import Socials from "@/components/Socials";
+import SwipeCards from "@/components/SwipeCards";
+import { Button } from "@/components/ui/button";
+import { getPosts } from "@/lib/posts";
+import {
+  ArrowDown,
+  ArrowDownRight,
+  ArrowRightIcon,
+  FileDown,
+} from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+const TED_BIRTH_YEAR = 1997;
+const LIMIT = 2; // max show 2
+
+export default async function Home() {
+  // const posts = (await getPosts())
+  //     .filter((post) => !post.draft)
+  //     .slice(0, LIMIT);
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      <article className="mt-8 flex flex-col gap-16 pb-16">
+        <section className="flex flex-col items-start gap-8 md:items-center">
+          {/*md:justify-between md:flex-row-reverse*/}
+          {/*<SwipeCards className="md:mr-8" />*/}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div className="flex max-w-[320px] flex-col sm:max-w-full">
+            <h1 className="title text-balance text-4xl sm:text-5xl">
+              Hi, Bryan here. 👋
+            </h1>
+
+            <p className="mt-2 whitespace-nowrap text-sm font-medium sm:text-base">
+              {new Date().getFullYear() - TED_BIRTH_YEAR}
+              yo IT Engineer based in Japan.
+            </p>
+
+            <p className="mt-4 max-w-sm text-balance text-sm sm:text-base">
+              Day job's main focus is automation, however my passion lies in anything IT related.
+            </p>
+
+            {/*<div className="mt-6 flex items-center gap-1">*/}
+            {/*  <p className="text-balance text-sm font-semibold sm:text-base">*/}
+            {/*    For Q&A, start a chat with Ted Support*/}
+            {/*  </p>*/}
+            {/*  <ArrowDownRight className="hidden size-5 animate-bounce sm:block" />*/}
+            {/*  <ArrowDown className="block size-5 animate-bounce sm:hidden" />*/}
+            {/*</div>*/}
+
+            {/*<p className="mt-1 text-xs font-light">*/}
+            {/*  For escalations, please find my*/}
+            {/*  <Link*/}
+            {/*      href="https://www.instagram.com/gomugomu.cat"*/}
+            {/*      target="_blank"*/}
+            {/*      className="link font-semibold"*/}
+            {/*      title="meow"*/}
+            {/*  >*/}
+            {/*    &nbsp;Ted Lead&nbsp;*/}
+            {/*  </Link>*/}
+            {/*  instead.*/}
+            {/*</p>*/}
+
+            <section className="mt-6 flex flex-wrap items-center gap-4">
+              <Link href="https://public.selubi.tech/personal/resume-english.pdf" target="_blank">
+                <Button variant="outline">
+                  <span className="font-semibold">Resume</span>
+                  <FileDown className="ml-2 size-5" />
+                </Button>
+              </Link>
+              <Socials />
+            </section>
+          </div>
+        </section>
+
+        {/*<Experience />*/}
+
+        {/*<section className="flex flex-col gap-8">*/}
+        {/*  <div className="flex justify-between">*/}
+        {/*    <h2 className="title text-2xl sm:text-3xl">featured projects</h2>*/}
+        {/*    <LinkWithIcon*/}
+        {/*      href="/projects"*/}
+        {/*      position="right"*/}
+        {/*      icon={<ArrowRightIcon className="size-5" />}*/}
+        {/*      text="view more"*/}
+        {/*    />*/}
+        {/*  </div>*/}
+        {/*  <Projects limit={LIMIT} />*/}
+        {/*</section>*/}
+
+        {/*<section className="flex flex-col gap-8">*/}
+        {/*  <div className="flex justify-between">*/}
+        {/*    <h2 className="title text-3xl">recent posts</h2>*/}
+        {/*    <LinkWithIcon*/}
+        {/*      href="/blog"*/}
+        {/*      position="right"*/}
+        {/*      icon={<ArrowRightIcon className="size-5" />}*/}
+        {/*      text="view more"*/}
+        {/*    />*/}
+        {/*  </div>*/}
+        {/*  <Posts posts={posts} />*/}
+        {/*</section>*/}
+      </article>
   );
 }
